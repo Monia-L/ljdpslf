b69c044044bc8587c82caa37f834d52126b6ff0f
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-04-24 13:22:17 +0200

    Use UUID to identify games

--- api/database/games.ts
+++ api/database/games.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { getCollection } from '.';
 
 type Game = {
@@ -6,7 +8,7 @@ type Game = {
 
 const createGame = async (): Promise<Game> => {
   const gamesCollection = await getCollection('games');
-  const response = await gamesCollection.insertOne({});
+  const response = await gamesCollection.insertOne({ id: uuidv4() });
   return response.ops[0];
 };
 
