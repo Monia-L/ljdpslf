f47eced3c3c8d8907838cfa0a0e2bd297decaf25
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-04-27 20:26:58 +0200

    _Debug: Log database URL when connecting to it

--- pages/api/_database/index.ts
+++ pages/api/_database/index.ts
@@ -3,7 +3,7 @@ import { MongoClient, Db, Collection } from 'mongodb';
 let client = null;
 const getDatabase = async (): Promise<Db> => {
   if (!client) {
-    console.log('New connection to Mongo');
+    console.log('New connection to Mongo', process.env.DATABASE_URL);
     client = await MongoClient.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
