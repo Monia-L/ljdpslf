246442de9f26428c92d107017a6d6aff3b7c93e8
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-05-03 21:39:58 +0200

    Render loading indicator instead of button when loading
    Better shows progress status rather than greying out button

--- lib/components/global/Button.tsx
+++ lib/components/global/Button.tsx
@@ -1,26 +1,31 @@
 import PropTypes from 'prop-types';
+import LoadingIndicator from './LoadingIndicator';
 
-const Button = ({ children, type, isLoading, onClick }): JSX.Element => (
-  <button type={type} disabled={isLoading} onClick={onClick}>
-    {children}
+const Button = ({ children, type, isLoading, onClick }): JSX.Element =>
+  isLoading ? (
+    <LoadingIndicator />
+  ) : (
+    <button type={type} disabled={isLoading} onClick={onClick}>
+      {children}
 
-    <style jsx>{`
-      button {
-        font-size: 18px;
-        color: black;
-        padding: 8px;
-        margin: 0;
-        border: 4px solid black;
-        background: ${isLoading ? 'lightgray' : 'white'};
-        font-weight: bold;
-      }
+      <style jsx>{`
+        button {
+          font-size: 16px;
+          color: black;
+          padding: 9px;
+          margin: 0;
+          border: 3px solid black;
+          background: ${isLoading ? 'lightgray' : 'white'};
+          font-weight: bold;
+          text-transform: uppercase;
+        }
 
-      button:active {
-        background: lightgray;
-      }
-    `}</style>
-  </button>
-);
+        button:active {
+          background: lightgray;
+        }
+      `}</style>
+    </button>
+  );
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
