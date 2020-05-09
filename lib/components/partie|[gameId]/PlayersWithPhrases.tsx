53121b3c309350728fd3f9ea128d2be18d00f96f
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-05-09 16:59:30 +0200

    Add label befor player whose turn it is to guess

--- lib/components/partie|[gameId]/PlayersWithPhrases.tsx
+++ lib/components/partie|[gameId]/PlayersWithPhrases.tsx
@@ -6,17 +6,24 @@ const PlayersWithPhrases = ({
   players: Array<TPlayerForPlayer>;
 }): JSX.Element => (
   <ul>
-    {players.map(({ isMe, id, name, phraseToGuess }) => (
+    {players.map(({ isMe, id, name, phraseToGuess, isTheirTurnToGuess }) => (
       <li key={id}>
-        {isMe ? (
-          <>
-            Vous êtes <b>???</b>
-          </>
-        ) : (
-          <>
-            {name} est <b>{phraseToGuess || '…'}</b>
-          </>
+        {isTheirTurnToGuess && (
+          <span className="label-turn-to-guess">{`À ${
+            isMe ? 'vous' : name
+          } de deviner`}</span>
         )}
+        <div className="name-and-phrase">
+          {isMe ? (
+            <>
+              Vous êtes <b>???</b>
+            </>
+          ) : (
+            <>
+              {name} est <b>{phraseToGuess || '…'}</b>
+            </>
+          )}
+        </div>
       </li>
     ))}
 
@@ -25,9 +32,19 @@ const PlayersWithPhrases = ({
         list-style: none;
         display: grid;
         grid-gap: 14px;
+        text-align: left;
       }
 
-      li {
+      .label-turn-to-guess {
+        background-color: lightgray;
+        display: inline-block;
+        padding: 6px 10px;
+        font-size: 18px;
+        font-variant: all-small-caps;
+        font-style: italic;
+      }
+
+      .name-and-phrase {
         border: 3px solid lightgray;
         padding: 8px 10px;
         background: #ffff99;
