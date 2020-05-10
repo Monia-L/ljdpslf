743ea27bcb9dc1d74152fe0175aac462994c9655
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-05-10 17:59:35 +0200

    test sending Pusher event directly from business function

--- pages/api/_lib/database/games.ts
+++ pages/api/_lib/database/games.ts
@@ -10,6 +10,21 @@ import {
 import { GamePhase, TGameDatabase } from '../../../../types';
 import { sendGameUpdateToPlayers } from '../client-subscriptions/games';
 
+import Channels from 'pusher';
+
+const pusherChannelsClient = new Channels({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+});
+
+const sendData = (channel: string, event: string, data: object): void => {
+  pusherChannelsClient.trigger(channel, event, data, () => {
+    console.log('event sent', data);
+  });
+};
+
 const createGame = async (ownerSessionId: string): Promise<TGameDatabase> => {
   const gamesCollection = await getCollection('games');
   const playerId = uuidv4();
@@ -43,6 +58,8 @@ const registerPlayer = async (
         { returnOriginal: false }
       )
     ).value;
+    console.log('registered host');
+    sendData('chan', 'evt', { example: 'donnee' });
     sendGameUpdateToPlayers(updatedGame);
     return updatedGame;
   } else {
